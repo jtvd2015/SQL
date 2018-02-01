dd705e62caaa4ad9dcd2f1ea295b2dd40c6f1110
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-01 12:00:03 -0500

    first commit

---
+++ ToDo Assignment.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ToDo (
+	Id int primary key identity(1,1),
+	Task nvarchar(80) not null, 
+	Priority int not null default 5
+		check (Priority > 0 and Priority < 10),
+	Completed bit not null default 0,
+	DueDate datetime,
+	
+)
+go
+insert into [ToDo] (Task, DueDate, Priority)
+	values ('Post 219 Web Project', '2018-FEB-03', 1)
+insert into [ToDo] (Task, DueDate, Priority)
+	values ('Eat lunch', '2018-FEB-01', 4)
+
+select * from ToDo
