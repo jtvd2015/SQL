786a2162c90de20978a9cbeb383e165038deb541
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-01 14:49:47 -0500

    first commit; created Corporate DB and used CorporateTBL

---
+++ corporateDBCorporateTBL.sql
@@ -0,0 +1,12 @@
+use master
+go
+drop database if exists Corporate
+go
+create database Corporate
+go
+use Corporate
+go
+drop table if exists Employee
+drop table if exists Manager
+drop table if exists Department
+go
